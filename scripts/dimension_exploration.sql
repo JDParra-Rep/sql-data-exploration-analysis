e177ebdf208ce0e86b4187aaef0eb41cf595015e
Juan Parra <juan.dp9406g@gmail.com>
2026-05-04 15:50:09 -0500

    Create dimension_exploration.sql

---
+++ scripts/dimension_exploration.sql
@@ -0,0 +1,27 @@
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+
+-- Retrieve a list of unique countries and gender from which customers originate
+SELECT DISTINCT 
+    country,
+    gender
+FROM gold.dim_customers
+ORDER BY country, gender
+
+-- Retrieve a list of unique categories, subcategories, and products
+SELECT DISTINCT 
+    category, 
+    subcategory, 
+    product_name 
+FROM gold.dim_products
+ORDER BY category, subcategory, product_name;
